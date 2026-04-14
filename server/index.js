7124595db817b4d1c24f2e1f6c819fef0f89764f
KARUNARATHNA W M D D <dilshankarunarathnastudy@gmail.com>
2026-04-14 11:28:07 +0530

    CORS Updated!

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://urban-community.vercel.app"],
+    origin: ["http://localhost:5173", "https://urban-community.vercel.app", process.env.CLIENT_URL],
     credentials: true,
   }));
 
